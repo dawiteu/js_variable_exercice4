4ef29652b9a92175366900058e4b9de9239997c7
DawiT <Dawid@Tararuj.be>
2020-12-28 15:19:47 +0100

    fin partie 1 exo 2

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,24 @@ let pre = "Dawid";
 
 let ville = prompt('Entre le nom d\'une ville'); 
 
-alert(`ville: ${ville} `);
+//alert(`ville: ${ville} `);
 
 console.log(`${nom} ${pre}`);
+
+
+/// ex 2 
+
+
+let nom2 = "iPhone"; 
+let pre2 = "Apple"; 
+let vdep = "Anvers"; 
+let vdes = "Namur"; 
+
+console.log(`le voyage de ${vdep} vers ${vdes} est enregist au nom de ${pre2} ${nom2}`);
+
+document.body.innerHTML=`le voyage de ${vdep} vers ${vdes} est enregist au nom de ${pre2} ${nom2}`;
+
+
+
+
+
