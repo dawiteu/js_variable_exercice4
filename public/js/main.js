3b7ebd47c90ecdebc01c97955ee009d263b022a8
DawiT <Dawid@Tararuj.be>
2020-12-28 15:22:28 +0100

    fin

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ let pre = "Dawid";
 
 let ville = prompt('Entre le nom d\'une ville'); 
 
-//alert(`ville: ${ville} `);
+alert(`ville: ${ville} `);
 
 console.log(`${nom} ${pre}`);
 
@@ -18,9 +18,16 @@ let vdes = "Namur";
 
 console.log(`le voyage de ${vdep} vers ${vdes} est enregist au nom de ${pre2} ${nom2}`);
 
-document.body.innerHTML=`le voyage de ${vdep} vers ${vdes} est enregist au nom de ${pre2} ${nom2}`;
+//document.body.innerHTML=`le voyage de ${vdep} vers ${vdes} est enregist au nom de ${pre2} ${nom2}`;
 
 
 
+// ex 2 B 
+let nom3 = prompt ("Entre ton nom"); 
+let pre3 = prompt ("entre ton prenom");
+let vde1 = prompt ("Entre ville de dep"); 
+let vde2 = prompt ("entre ville de dest"); 
+
+console.log(`le voyage de ${vde1} vers ${vde2} est enregist au nom de ${pre3} ${nom3}`);
 
 
